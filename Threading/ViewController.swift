75b08a1b50fe3e522445dab7e3f8ce60f33f7c45
Şeyma Arslan <seymarslannn@gmail.com>
2023-10-22 16:31:32 +0300

    Initial Commit

---
+++ Threading/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Threading
+//
+//  Created by Seyma on 22.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
